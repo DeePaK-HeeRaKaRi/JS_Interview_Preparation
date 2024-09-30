0e215e513936dd79e21a0e9a5d03e4ac886eae2b
Deepak Heerakari <dkh9558@gmail.com>
2024-09-30 19:09:16 +0530

    Left & RIght Rotation of array

---
+++ DSA/Array/1-Left Rotate.js
@@ -0,0 +1,26 @@
+var rotate = function(nums, k) {
+    // if k=7 and n= 7 so no rotations are required
+    // if k = 8  and n=7 only 1 rotation is required
+    let n= nums.length
+    k = k%n  // k > n 
+
+    function reverse(nums,l,r) {
+        while(l<r){
+            [nums[l],nums[r]] = [nums[r],nums[l]]
+            l++
+            r--
+        }
+    }
+
+    reverse(nums,0,k-1)   
+    reverse(nums,k,n-1)
+    reverse(nums,0,n-1)
+
+    console.log(nums)
+
+};
+
+let nums = [1,2,3,4,5,6,7]
+let k = 2
+
+rotate(nums,k)
